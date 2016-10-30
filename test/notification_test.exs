601de1501e86ed303540f86e32e210b06e9e82c8
Nicholas Henry <nicholas.henry@gmail.com>
2016-10-30 18:16:05 -0400

    Subscribe a GenServer

--- test/notification_test.exs
+++ test/notification_test.exs
@@ -2,11 +2,51 @@ defmodule NotificationTest do
   use ExUnit.Case
   doctest Notification
 
-  alias Phoenix.PubSub
+  # worker(Subscriber, ["events:orders"])
+  defmodule Subscriber do
+    use GenServer
+
+    # Client API
+
+    def start_link(channel) do
+      GenServer.start_link(__MODULE__, channel)
+    end
+
+    # Server Callbacks
+
+    def init(channel) do
+      IO.inspect "Subscribing to #{channel}"
+      pid = self
+      ref = Phoenix.PubSub.subscribe(Notification.PubSub, channel)
+      {:ok, {pid, channel, ref}}
+    end
+
+    def handle_info({:order_shipped, _payload} = message, state) do
+      IO.inspect "#######################"
+      IO.inspect "Test - Received Message:"
+      IO.inspect message
+      IO.inspect "#######################"
+      {:noreply, state}
+    end
+
+    def handle_info(message, state) do
+      IO.inspect "#######################"
+      IO.inspect "Catch All - Received Message:"
+      IO.inspect message
+      IO.inspect "#######################"
+      {:noreply, state}
+    end
+  end
 
   test "learning pub sub" do
-     PubSub.subscribe Notification.PubSub, "order:123"
-     PubSub.broadcast Notification.PubSub, "order:123", {:order_shipped, %{id: 123, total: 10}}
-     assert {:order_shipped, %{id: 123, total: 10}} = hd(Process.info(self)[:messages])
+    Subscriber.start_link("events:orders")
+    Phoenix.PubSub.subscribe(Notification.PubSub, "events:orders")
+
+    Phoenix.PubSub.broadcast(Notification.PubSub, "events:orders", {:order_shipped, %{id: 123, total: 10}})
+    :timer.sleep 500
+
+    messages = Process.info(self)[:messages]
+    assert length(messages) == 1
+    assert {:order_shipped, %{id: 123, total: 10}} = hd(messages)
   end
 end
